459e8282d96e514b15ace3d104133b0aa7f2a015
Wendelin  Gerein <wgerein@tuev-nord.de>
2019-06-08 13:06:05 +0200

    test.js working create item[0].toUpperCase() directory true

--- a.js
+++ a.js
@@ -1,61 +1,28 @@
-const fs = require('fs')
+var path = require('path')
+var fs = require('fs')
 
-const dir = './rohfiles'
+function recFindByExt(base,ext,files,result) 
+{
+    files = files || fs.readdirSync(base) 
+    result = result || [] 
 
+    files.forEach( 
+        function (file) {
+            var newbase = path.join(base,file)
+            if ( fs.statSync(newbase).isDirectory() )
+            {
+                result = recFindByExt(newbase,ext,fs.readdirSync(newbase),result)
+            }
+            else
+            {
+                if ( file.substr(-1*(ext.length+1)) == '.' + ext )
+                {
+                    result.push(newbase)
+                } 
+            }
+        }
+    )
+    return result
+}
 
-var mkdir = function(dir) {
-	// making directory without exception if exists
-	try {
-		fs.mkdirSync(dir, 0755);
-	} catch(e) {
-		if(e.code != "EEXIST") {
-			throw e;
-		}
-	}
-};
-
-var rmdir = function(dir) {
-	if (path.existsSync(dir)) {
-		var list = fs.readdirSync(dir);
-		for(var i = 0; i < list.length; i++) {
-			var filename = path.join(dir, list[i]);
-			var stat = fs.statSync(filename);
-			
-			if(filename == "." || filename == "..") {
-				// pass these files
-			} else if(stat.isDirectory()) {
-				// rmdir recursively
-				rmdir(filename);
-			} else {
-				// rm fiilename
-				fs.unlinkSync(filename);
-			}
-		}
-		fs.rmdirSync(dir);
-	} else {
-		console.warn("warn: " + dir + " not exists");
-	}
-};
-
-var copyDir = function(src, dest) {
-	mkdir(dest);
-	var files = fs.readdirSync(src);
-	for(var i = 0; i < files.length; i++) {
-		var current = fs.lstatSync(path.join(src, files[i]));
-		if(current.isDirectory()) {
-			copyDir(path.join(src, files[i]), path.join(dest, files[i]));
-		} else if(current.isSymbolicLink()) {
-			var symlink = fs.readlinkSync(path.join(src, files[i]));
-			fs.symlinkSync(symlink, path.join(dest, files[i]));
-		} else {
-			copy(path.join(src, files[i]), path.join(dest, files[i]));
-		}
-	}
-};
-
-
-var copy = function(src, dest) {
-	var oldFile = fs.createReadStream(src);
-	var newFile = fs.createWriteStream(dest);
-	oldFile.pipe(newFile);
-};
+ext_file_list = recFindByExt('/mypath','ext')
