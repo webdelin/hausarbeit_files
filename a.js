995c6227355c542070f178ed7c802691000aebd5
Wendelin  Gerein <wgerein@tuev-nord.de>
2019-06-12 21:45:21 +0200

    a.js working async await

--- a.js
+++ a.js
@@ -4,87 +4,60 @@ const base = path.join(__dirname, './rohfiles');
 const fileDir = path.join(__dirname, './temp');
 
 
-  const writeDir = () => {
-    fs.readdir(base, (err, files) => {
-      if (err) {
-        console.log('Ошибка чтения каталога');
-      }
-      files.forEach((item) => {
-        let localBase = path.join(base, item)
-        let state = fs.statSync(localBase)
-        const dirsA = item[0].toUpperCase()
-        if (state.isDirectory()) {
-          console.log(item)
-        } else {
-          if (!fs.existsSync(fileDir)) {
-            for (i=0; i<dirsA.length; i++) {
-              fs.mkdir(fileDir + '/' + dirsA, { recursive: true }, (err) => {
-                if (err) throw err;
-              });
-            }
-          } else {
-
-            
-
+const writeDir = () => {
+  fs.readdir(base, (err, files) => {
+    if (err) {
+      console.log('Ошибка чтения каталога');
+    }
+    files.forEach((item) => {
+      const localBase = path.join(base, item)
+      const state = fs.statSync(localBase)
+      const dirsA = item[0].toUpperCase()
+      const dirsEx = path.parse(item.toUpperCase()).ext
+      const dirExFix = dirsEx.replace('.', '')
+      
+        if (!state.isDirectory() && !fs.existsSync(dirExFix)) {
+          for (i=0; i<dirExFix.length; i++) {
+            fs.mkdirSync(fileDir + '/' + dirsA + '/' + dirExFix, { recursive: true }, (err) => {
+              
+              if (err) {
+                return console.log('FOLDER: ' + err);
+              } 
+            })
+            return item
           }
         }
-        
-      })
     })
+  })
+}
 
-  }
-  const writeFile = () => {
-    fs.readdir(base, function (err, files) {
+const writeFile = () => {
+      fs.readdir(base, function (err, files) {
         if (err) {
             return console.log('Unable to scan directory: ' + err);
         } 
         files.forEach(function (file) {
-          let localBase = path.join(base, file)
-          let state = fs.statSync(localBase)
-            console.log(file)
-            if (state.isDirectory()) {
-            } else {
-            fs.link( base + '/' + file,  fileDir + '/' + file[0].toUpperCase() + '/' + file, err => {
-              console.log('FILE: ' + fileDir + '/' + file[0].toUpperCase() + '/' + file)
-              if (err) {
-                console.error(err.message)
-                return
-              }
+          const localBase = path.join(base, file)
+          const state = fs.statSync(localBase)
+          const dirsEx = path.parse(file.toUpperCase()).ext
+          const dirExFix = dirsEx.replace('.', '')
+          const exact = fileDir + '/' + file[0].toUpperCase() + '/' + dirExFix + '/' + file
+          if (!fs.existsSync(exact)) {
+            fs.linkSync( base + '/' + file,  exact, err => {
+             //console.log('FILE: ' + fileDir + '/' + file[0].toUpperCase() + '/' + file)
+            if (err) {
+              return console.log('FELE: ' + err);
+            } 
             })
           }
-        });
-    });
+        })
+    })
+  }
+
+  const getProducts = async () => {
+    const item = await writeDir()
   
+    return await writeFile()
   }
-  let getData = async () => {
-    try {
-        await writeDir();
-        await writeFile();
-      } catch (error) {
-        console.log(error);
-      }
-  };
-  if (fs.existsSync(fileDir)) {
-    const Q = require('q')
-    function rmdir(dir) {
-      return Q.nfcall(fs.access, dir).then(() => {
-        return Q.nfcall(fs.readdir, dir)
-          .then(files => files.reduce((pre, f) => pre.then(() => {
-            var sub = path.join(dir, f)
-            return Q.nfcall(fs.lstat, sub).then(stat => {
-              if (stat.isDirectory()) return rmdir(sub)
-              return Q.nfcall(fs.unlink, sub)
-            })
-          }), Q()))
-      }, err => {})
-      .then(() => Q.nfcall(fs.rmdir, dir))
-    }
-    rmdir(fileDir)
-    getData()
-    
-  }else {
-    getData()
-  }
-
-
-
+  
+  getProducts()
